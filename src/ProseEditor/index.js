f4f297a657bf9cefbf30ff4a326a5790f65587d1
Rainke <woshihepeng520@163.com>
2025-12-06 23:11:01 +0800

    feat: 支持公式

--- src/ProseEditor/index.js
+++ src/ProseEditor/index.js
@@ -1,34 +1,38 @@
-import { EditorView } from "prosemirror-view"
-import { EditorState } from 'prosemirror-state'
-import { Schema, DOMParser } from 'prosemirror-model'
-import { schema, defaultMarkdownParser, defaultMarkdownSerializer } from "prosemirror-markdown"
-import { exampleSetup } from 'prosemirror-example-setup'
-import 'prosemirror-view/style/prosemirror.css'
-import { purplePlugin } from "./plugins/purple"
+import { EditorView } from 'prosemirror-view';
+import { EditorState } from 'prosemirror-state';
+import { Schema, DOMParser } from 'prosemirror-model';
+import { defaultMarkdownParser, defaultMarkdownSerializer } from 'prosemirror-markdown';
+import { exampleSetup } from 'prosemirror-example-setup';
+import 'prosemirror-view/style/prosemirror.css';
+import { purplePlugin } from './plugins/purple';
+import { schema } from './schema';
+import 'katex/dist/katex.min.css';
 
-console.log(schema)
-export const createEditor = (dom, {markdown, onStateChange}) => {
-  const state =  EditorState.create({
-    doc: defaultMarkdownParser.parse(markdown),
-    plugins: exampleSetup({ schema }).concat([
-      purplePlugin
-    ])
-  })
+export const createEditor = (dom, { markdown, onStateChange }) => {
+  const state = EditorState.create({
+    doc: schema.node('doc', null, [
+      schema.node('paragraph', null, [schema.text('paragraph')]),
+      schema.node('horizontal_rule'),
+      schema.node('paragraph', null, [
+        schema.text('公式: '),
+        schema.node('math_inline', { value: 'x^2' }),
+      ]),
+      schema.node('math_block', { value: '\\frac{a}{b} = c' }),
+    ]),
+    plugins: [],
+  });
   let view = new EditorView(dom, {
     state,
     dispatchTransaction(transaction) {
-      console.log(
-        '📝 文档变化:',
-        transaction
-      )
+      console.log('📝 文档变化:', transaction);
       // 应用事务，获得新状态
-      const newState = view.state.apply(transaction)
+      const newState = view.state.apply(transaction);
       // 更新视图
-      view.updateState(newState)
-      newState.view = view
-      onStateChange?.(newState)
-    }
-  })
-  window.view = view
-  return view
-}
+      view.updateState(newState);
+      newState.view = view;
+      onStateChange?.(newState);
+    },
+  });
+  window.view = view;
+  return view;
+};
--- src/ProseEditor/schema.js
+++ src/ProseEditor/schema.js
@@ -1,11 +1,263 @@
-import { EditorState } from 'prosemirror-state'
-import { Schema, DOMParser } from 'prosemirror-model'
-import { schema as basicSchema } from 'prosemirror-schema-basic'
-import { addListNodes } from 'prosemirror-schema-list'
+import { addListNodes } from 'prosemirror-schema-list';
+import { Schema } from 'prosemirror-model';
+import katex from 'katex'
 
-const schema = new Schema({
-  // 从基础 schema 继承节点，并添加列表节点
-  nodes: addListNodes(basicSchema.spec.nodes, 'paragraph block*', 'block'),
-  // 直接使用基础 schema 的标记（加粗、斜体、链接、代码）
-  marks: basicSchema.spec.marks
-})
+
+/// Document schema for the data model used by CommonMark.
+export const schema = new Schema({
+  nodes: {
+    doc: {
+      content: 'block+',
+    },
+
+    paragraph: {
+      content: 'inline*',
+      group: 'block',
+      parseDOM: [{ tag: 'p' }],
+      toDOM() {
+        return ['p', 0];
+      },
+    },
+
+    blockquote: {
+      content: 'block+',
+      group: 'block',
+      parseDOM: [{ tag: 'blockquote' }],
+      toDOM() {
+        return ['blockquote', 0];
+      },
+    },
+
+    horizontal_rule: {
+      group: 'block',
+      parseDOM: [{ tag: 'hr' }],
+      toDOM() {
+        return ['div', ['hr']];
+      },
+    },
+
+    heading: {
+      attrs: { level: { default: 1 } },
+      content: '(text | image)*',
+      group: 'block',
+      defining: true,
+      parseDOM: [
+        { tag: 'h1', attrs: { level: 1 } },
+        { tag: 'h2', attrs: { level: 2 } },
+        { tag: 'h3', attrs: { level: 3 } },
+        { tag: 'h4', attrs: { level: 4 } },
+        { tag: 'h5', attrs: { level: 5 } },
+        { tag: 'h6', attrs: { level: 6 } },
+      ],
+      toDOM(node) {
+        return ['h' + node.attrs.level, 0];
+      },
+    },
+
+    code_block: {
+      content: 'text*',
+      group: 'block',
+      code: true,
+      defining: true,
+      marks: '',
+      attrs: { params: { default: '' } },
+      parseDOM: [
+        {
+          tag: 'pre',
+          preserveWhitespace: 'full',
+          getAttrs: (node) => ({ params: node.getAttribute('data-params') || '' }),
+        },
+      ],
+      toDOM(node) {
+        return ['pre', node.attrs.params ? { 'data-params': node.attrs.params } : {}, ['code', 0]];
+      },
+    },
+
+    ordered_list: {
+      content: 'list_item+',
+      group: 'block',
+      attrs: { order: { default: 1 }, tight: { default: false } },
+      parseDOM: [
+        {
+          tag: 'ol',
+          getAttrs(dom) {
+            return {
+              order: dom.hasAttribute('start') ? +dom.getAttribute('start') : 1,
+              tight: dom.hasAttribute('data-tight'),
+            };
+          },
+        },
+      ],
+      toDOM(node) {
+        return [
+          'ol',
+          { start: node.attrs.order == 1 ? null : node.attrs.order, 'data-tight': node.attrs.tight ? 'true' : null },
+          0,
+        ];
+      },
+    },
+
+    bullet_list: {
+      content: 'list_item+',
+      group: 'block',
+      attrs: { tight: { default: false } },
+      parseDOM: [{ tag: 'ul', getAttrs: (dom) => ({ tight: dom.hasAttribute('data-tight') }) }],
+      toDOM(node) {
+        return ['ul', { 'data-tight': node.attrs.tight ? 'true' : null }, 0];
+      },
+    },
+
+    list_item: {
+      content: 'block+',
+      defining: true,
+      parseDOM: [{ tag: 'li' }],
+      toDOM() {
+        return ['li', 0];
+      },
+    },
+
+    text: {
+      group: 'inline',
+    },
+    image: {
+      inline: true,
+      attrs: {
+        src: {},
+        alt: { default: null },
+        title: { default: null },
+      },
+      group: 'inline',
+      draggable: true,
+      parseDOM: [
+        {
+          tag: 'img[src]',
+          getAttrs(dom) {
+            return {
+              src: dom.getAttribute('src'),
+              title: dom.getAttribute('title'),
+              alt: dom.getAttribute('alt'),
+            };
+          },
+        },
+      ],
+      toDOM(node) {
+        return ['img', node.attrs];
+      },
+    },
+
+    hard_break: {
+      inline: true,
+      group: 'inline',
+      selectable: false,
+      parseDOM: [{ tag: 'br' }],
+      toDOM() {
+        return ['br'];
+      },
+    },
+    math_inline: {
+      group: 'inline',
+      inline: true,
+      draggable: true,
+      atom: true,
+      attrs: {
+        value: {
+          default: '',
+        },
+      },
+      parseDOM: [
+        {
+          tag: 'span.math-inline',
+          getAttrs(dom) {
+            return { value: dom.dataset.value };
+          },
+        },
+      ],
+      toDOM(node) {
+        const dom = document.createElement('span');
+        dom.className = 'math-inline';
+        dom.dataset.value = node.attrs.value;
+        katex.render(node.attrs.value, dom, { throwOnError: false });
+        return dom;
+      }
+    },
+    math_block: {
+      group: 'block',
+      inline: false,
+      draggable: true,
+      atom: true,
+      attrs: {
+        value: {
+          default: ''
+        }
+      },
+      parseDOM: [
+        {
+          tag: 'div.math-block',
+          getAttrs(dom) {
+            return { value: dom.dataset.value };
+          },
+        },
+      ],
+      toDOM(node) {
+        const dom = document.createElement('div');
+        dom.className = 'math-block';
+        dom.dataset.value = node.attrs.value;
+        katex.render(node.attrs.value, dom, { throwOnError: false, displayMode: true });
+        return dom;
+      }
+    }
+  },
+
+  marks: {
+    em: {
+      parseDOM: [
+        { tag: 'i' },
+        { tag: 'em' },
+        { style: 'font-style=italic' },
+        { style: 'font-style=normal', clearMark: (m) => m.type.name == 'em' },
+      ],
+      toDOM() {
+        return ['em'];
+      },
+    },
+
+    strong: {
+      parseDOM: [
+        { tag: 'strong' },
+        { tag: 'b', getAttrs: (node) => node.style.fontWeight != 'normal' && null },
+        { style: 'font-weight=400', clearMark: (m) => m.type.name == 'strong' },
+        { style: 'font-weight', getAttrs: (value) => /^(bold(er)?|[5-9]\d{2,})$/.test(value) && null },
+      ],
+      toDOM() {
+        return ['strong'];
+      },
+    },
+
+    link: {
+      attrs: {
+        href: {},
+        title: { default: null },
+      },
+      inclusive: false,
+      parseDOM: [
+        {
+          tag: 'a[href]',
+          getAttrs(dom) {
+            return { href: dom.getAttribute('href'), title: dom.getAttribute('title') };
+          },
+        },
+      ],
+      toDOM(node) {
+        return ['a', node.attrs];
+      },
+    },
+
+    code: {
+      code: true,
+      parseDOM: [{ tag: 'code' }],
+      toDOM() {
+        return ['code'];
+      },
+    },
+  },
+});
